72ac95861a54106c914d98bfe7a0a865fb40fed2
Elizabeth Blair <blair_pkfh@MonkadooCentral.(none)>
2026-09-23 22:25:01 -0600

    Forgot to check if the piece is in bounds. Hopefully I can get it working again.

--- shared/src/main/java/chess/HoppingMovesCalculator.java
+++ shared/src/main/java/chess/HoppingMovesCalculator.java
@@ -14,12 +14,13 @@ public class HoppingMovesCalculator implements PieceMovesCalculator{
             int rowOffset = offset[0];
             int colOffset = offset[1];
             ChessPosition potentialPos = new ChessPosition(myPosition.getRow() + rowOffset, myPosition.getColumn() + colOffset );
-
-            if(board.getPiece(potentialPos) == null){ //If the vision space is clear
-                legalMoves.add(new ChessMove(myPosition, potentialPos, null));
-            } else if(board.getPiece(potentialPos) != null && board.getPiece(potentialPos).getTeamColor() != myPiece.getTeamColor()){
-                //If the vision space is not clear BUT we can capture it.
-                legalMoves.add(new ChessMove(myPosition, potentialPos, null));
+            if(potentialPos.isInBounds()){
+                if(board.getPiece(potentialPos) == null){ //If the vision space is clear
+                    legalMoves.add(new ChessMove(myPosition, potentialPos, null));
+                } else if(board.getPiece(potentialPos) != null && board.getPiece(potentialPos).getTeamColor() != myPiece.getTeamColor()){
+                    //If the vision space is not clear BUT we can capture it.
+                    legalMoves.add(new ChessMove(myPosition, potentialPos, null));
+                }
             }
         }
         return legalMoves;
