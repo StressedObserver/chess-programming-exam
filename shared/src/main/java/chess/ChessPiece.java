2592cb41e03e4c15474c71f8ef366592d7694c51
Elizabeth Blair <blair_pkfh@MonkadooCentral.(none)>
2026-09-23 22:23:32 -0600

    Hopefully got the HoppingMovesCalculator.java working. About to run the tests for them to see.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -67,8 +67,13 @@ public class ChessPiece {
                 SlidingMovesCalculator bishopSeer = new SlidingMovesCalculator();
                 return bishopSeer.possibleMoves(board, myPosition, bishopOffsets);
             case KING:
-
+                int[][] kingOffsets = {{1,0}, {-1, 0}, {0, 1}, {0,-1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}}; //Same as queen's just used differently.
+                HoppingMovesCalculator kingSeer = new HoppingMovesCalculator();
+                return kingSeer.possibleMoves(board, myPosition, kingOffsets);
             case KNIGHT:
+                int[][] knightOffsets = {{2,1}, {-2, 1}, {1, 2}, {1,-2}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}}; //This is where it gets a bit weird.
+                HoppingMovesCalculator knightSeer = new HoppingMovesCalculator();
+                return  knightSeer.possibleMoves(board, myPosition, knightOffsets);
 
             case PAWN:
 
---
+++ shared/src/main/java/chess/HoppingMovesCalculator.java
@@ -0,0 +1,27 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class HoppingMovesCalculator implements PieceMovesCalculator{
+
+    @Override
+    public Collection<ChessMove> possibleMoves(ChessBoard board, ChessPosition myPosition, int[][] offsets) {
+        ChessPiece myPiece = board.getPiece(myPosition);
+        Collection<ChessMove> legalMoves = new ArrayList<>(); //Change this later when function is fully implemented.
+        for(int[] offset : offsets){
+            int rowOffset = offset[0];
+            int colOffset = offset[1];
+            ChessPosition potentialPos = new ChessPosition(myPosition.getRow() + rowOffset, myPosition.getColumn() + colOffset );
+
+            if(board.getPiece(potentialPos) == null){ //If the vision space is clear
+                legalMoves.add(new ChessMove(myPosition, potentialPos, null));
+            } else if(board.getPiece(potentialPos) != null && board.getPiece(potentialPos).getTeamColor() != myPiece.getTeamColor()){
+                //If the vision space is not clear BUT we can capture it.
+                legalMoves.add(new ChessMove(myPosition, potentialPos, null));
+            }
+        }
+        return legalMoves;
+    }
+}
