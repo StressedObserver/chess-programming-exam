51aa1e6c4548a6ad6db40d17e7402c8943537991
Elizabeth Blair <blair_pkfh@MonkadooCentral.(none)>
2026-09-23 22:12:00 -0600

    Forgot to fix the initialization of legalMoves. Hopefully I fixed it now.

--- shared/src/main/java/chess/SlidingMovesCalculator.java
+++ shared/src/main/java/chess/SlidingMovesCalculator.java
@@ -8,7 +8,7 @@ public class SlidingMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> possibleMoves(ChessBoard board, ChessPosition myPosition, int[][] offsets) {
         ChessPiece myPiece = board.getPiece(myPosition);
-        Collection<ChessMove> legalMoves = List.of(); //Change this later when function is fully implemented.
+        Collection<ChessMove> legalMoves; //Change this later when function is fully implemented.
         for(int[] offset : offsets){
             int rowOffset = offset[0];
             int colOffset = offset[1];
